36535244f91071c9c31317bc274dd7c21113c740
Julio Cesar <juliocesargualberto617@gmail.com>
2024-12-13 12:41:52 -0300

    test:user not found error

--- financeManager.Tests/Services/AuthServiceTest.cs
+++ financeManager.Tests/Services/AuthServiceTest.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoFixture;
+using FinanceManager.Application.Services;
+using FinanceManager.Domain.Model;
+using FinanceManager.Infrastructure.Repositories.@interface;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +13,27 @@ namespace FinanceManager.Tests.Services
 {
     public class AuthServiceTest
     {
+        private readonly Mock<IUserRepositories> repository;
+        private readonly Fixture fixture;
+
+        public AuthServiceTest()
+        {
+            this.repository = new Mock<IUserRepositories>();
+            this.fixture = new Fixture();
+        }
+        public async Task ShouldBeReturnInvalidEmailInDatabase()
+        {
+            var InputNotExistEmail = fixture.Create<Login>();
+
+            repository.Setup(i => i.GetUserByEmail(InputNotExistEmail.Email)).ReturnsAsync((User?) null);
+
+            var authService = new AuthService(repository.Object);
+
+            var errorEmailIsNotFound = Assert.ThrowsAsync <Exception>(async () =>
+            {
+                throw new Exception("something is wrong");
+            });
+        }
 
     }
 }
