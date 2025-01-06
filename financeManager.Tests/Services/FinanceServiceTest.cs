40dd40a22989624dc8ae7ab756774bbcb4156c90
Julio Cesar <juliocesargualberto617@gmail.com>
2025-01-06 11:12:18 -0300

    test: test in case success to create a finance service

--- financeManager.Tests/Services/FinanceServiceTest.cs
+++ financeManager.Tests/Services/FinanceServiceTest.cs
@@ -1,4 +1,6 @@
 ﻿using AutoFixture;
+using AutoMapper;
+using FinanceManager.Application.Mapping;
 using FinanceManager.Application.Services;
 using FinanceManager.Domain.Model;
 using FinanceManager.Infrastructure.Repositories;
@@ -16,7 +18,7 @@ namespace FinanceManager.Tests.Services
         public FinanceServiceTest()
         {
             this.repository = new Mock<IFinanceRepository>();
-            this.fixture = new Fixture();
+            this.fixture = new Fixture();        
         }
 
         [Fact]
@@ -39,6 +41,34 @@ namespace FinanceManager.Tests.Services
             var exception = await Assert.ThrowsAsync<InvalidEnumArgumentException>(() => service.CreateUserFinance(InputWithInvalidServiceFinance));
 
             Assert.Equal("type finance not exists", exception.Message);
+        }
+        [Fact]
+
+        public async Task ShouldBeReturnSuccessInCaseAllDataAreCorrectly()
+        {
+            var InputWithValidInput = fixture.Build<Finance>()
+                .With(f => f.Type, (TypeFinance)1)
+                .With(f => f.Id, 0)
+                .Create();
+
+
+            var InputWithValidServiceFinance = fixture.Build<CreateFinanceDto>()
+                .With(f => f.Type, (FinanceType)InputWithValidInput.Type)
+                .With(f => f.Status, InputWithValidInput.Status)
+                .With(f => f.Name, InputWithValidInput.Name)
+                .With(f => f.UserId, InputWithValidInput.UserId)
+                .With(f => f.Quantity, InputWithValidInput.Quantity)
+                .Create();
+
+            repository.Setup(i => i.CreateUserFinance(InputWithValidInput)).ReturnsAsync(InputWithValidInput.Id);
+
+            var service = new FinanceService(repository.Object);
+            
+            var SuccessCreateFinance = await service.CreateUserFinance(InputWithValidServiceFinance);
+
+            Console.WriteLine(SuccessCreateFinance + " " +  InputWithValidInput);
+
+            Assert.Equal(InputWithValidInput.Id, SuccessCreateFinance);
         }
 
     }
